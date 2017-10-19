430d4805e7c74c3d3f86eb2d19309c09c943f0a4
jondeaton <jonpauldeaton@gmail.com>
2017-10-19 09:45:54 -0700

    Got basic command line parsing working

--- src/main-distributed.cpp
+++ src/main-distributed.cpp
@@ -57,21 +57,22 @@ void parse_command_line_options(int argc, const char* const* argv) {
 
   po::options_description log_options("Logging");
   log_options.add_options()
-    ("verbose,v",   "Verbose output")
-    ("debug,debug", "Debug output");
+    ("verbose,v",   po::bool_switch(&verbose), "Verbose output")
+    ("debug,debug", po::bool_switch(&debug), "Debug output");
 
   po::options_description config("Config");
   config.add_options()
     ("regex,r", po::value<string>()->default_value("*"),      "file pattern regular expression")
     ("k,k",     po::value<int>(&k)->default_value(K_DEFAULT), "k-mer size (i.e. \"k\")");
 
+
   po::options_description hidden("Hidden");
   hidden.add_options()
-    ("input-directory", po::value<string>(), "directory to process from")
-    ("output-file",     po::value<string>(), "output file");
+    ("input-directory", po::value<string>(&input_directory), "directory to process from")
+    ("output-file", po::value<string>(&output_file));
 
   po::positional_options_description p;
-  p.add("input-directory", -1);
+  p.add("input-directory", 1).add("output-file", 1);
 
   po::options_description desc("Distributed k-mer counter options");
   desc.add(info).add(log_options).add(config);
@@ -97,14 +98,14 @@ void parse_command_line_options(int argc, const char* const* argv) {
   }
 
   // Set globals
-  verbose = (bool) vm.count("verbose");
-  debug = (bool) vm.count("debug");
   try {
     file_regex = vm["regex"].as<string>();
 
     cout << "trying things" << endl;
 
-    auto shit = vm["input-directory"].as<vector<string>>(); // <-- why the fuck doesn't this work
+
+    cout << input_directory << endl;
+    cout << output_file << endl;
 
     cout << "MADE IT THIS FAR @@@@@@@" << endl; exit(69);
   } catch (int e) {
