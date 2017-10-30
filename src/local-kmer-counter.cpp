e110690f60f390a75d57325b729ee7630d01b58d
Jon Deaton <jonpauldeaton@gmail.com>
2017-10-29 21:14:59 -0700

    Log linking works now with addition of -DBOOST_LOG_DYN_LINK flag

--- src/local-kmer-counter.cpp
+++ src/local-kmer-counter.cpp
@@ -8,11 +8,27 @@
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 
-//#include <boost/smart_ptr/shared_ptr.hpp>
-//#include <boost/smart_ptr/make_shared_object.hpp>
-//#include <boost/log/sinks.hpp>
-//#include <boost/log/trivial.hpp>
-//#include <boost/log/expressions.hpp>
+#include <boost/log/sinks.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <memory>
+
+
+#   include <boost/date_time/posix_time/posix_time_types.hpp>
+#   include <boost/log/trivial.hpp>
+#   include <boost/log/core.hpp>
+#   include <boost/log/expressions.hpp>
+#   include <boost/log/sources/logger.hpp>
+#   include <boost/log/utility/setup/file.hpp>
+#   include <boost/log/utility/setup/console.hpp>
+#   include <boost/log/utility/setup/common_attributes.hpp>
+#   include <boost/log/support/date_time.hpp>
+#   include <boost/log/sinks/sync_frontend.hpp>
+#   include <boost/log/sinks/text_file_backend.hpp>
+#   include <boost/log/sinks/text_ostream_backend.hpp>
+#   include <boost/log/attributes/named_scope.hpp>
+
+
 
 #define NUM_THREADS 8
 #define K_DEFAULT 4
@@ -21,9 +37,9 @@
 namespace po = boost::program_options;
 namespace fs = boost::filesystem;
 
-//namespace logging = boost::log;
-//namespace sinks = boost::log::sinks;
-//namespace expr = boost::log::expressions;
+namespace logging = boost::log;
+namespace sinks = boost::log::sinks;
+namespace expr = boost::log::expressions;
 
 using namespace std;
 
@@ -77,28 +93,43 @@ void LocalKmerCounter::setup_streams() {
  * Sets up the logger for this object
  */
 void LocalKmerCounter::setup_logging() {
+  boost::log::add_common_attributes();
+  boost::log::core::get()->add_global_attribute("Scope",
+                                                boost::log::attributes::named_scope());
+  boost::log::core::get()->set_filter(
+    boost::log::trivial::severity >= boost::log::trivial::trace
+  );
 
-//  typedef sinks::synchronous_sink<sinks::text_ostream_backend> text_sink;
-//  shared_ptr<text_sink> sink = make_shared<text_sink>();
-//
-//  if (to_stdout) sink->locked_backend()->add_stream(make_shared<ofstream>("kmers.log"));
-//  else sink->locked_backend()->add_stream(make_shared<ostream>(cout));
+  /* log formatter:
+   * [TimeStamp] [ThreadId] [Severity Level] [Scope] Log message
+   */
+  auto fmtTimeStamp = boost::log::expressions::
+  format_date_time<boost::posix_time::ptime>("TimeStamp", "%Y-%m-%d %H:%M:%S.%f");
+  auto fmtThreadId = boost::log::expressions::
+  attr<boost::log::attributes::current_thread_id::value_type>("ThreadID");
+  auto fmtSeverity = boost::log::expressions::
+  attr<boost::log::trivial::severity_level>("Severity");
+  auto fmtScope = boost::log::expressions::format_named_scope("Scope",
+                                                              boost::log::keywords::format = "%n(%f:%l)",
+                                                              boost::log::keywords::iteration = boost::log::expressions::reverse,
+                                                              boost::log::keywords::depth = 2);
+  boost::log::formatter logFmt =
+    boost::log::expressions::format("[%1%] (%2%) [%3%] [%4%] %5%")
+    % fmtTimeStamp % fmtThreadId % fmtSeverity % fmtScope
+    % boost::log::expressions::smessage;
 
-//  typedef sinks::synchronous_sink< sinks::text_ostream_backend > text_sink;
-//  boost::shared_ptr< text_sink > sink = boost::make_shared<text_sink>();
-//
-//  sink->locked_backend()->add_stream(
-//    boost::make_shared< std::ofstream >("sample.log"));
-//
-//  sink->set_formatter
-//    (
-//      expr::format("%1%: <%2%> %3%")
-//      % expr::attr<unsigned int>("LineID")
-//      % logging::trivial::severity
-//      % expr::smessage
-//    );
-//
-//  logging::core::get()->add_sink(sink);
+  /* console sink */
+  auto consoleSink = boost::log::add_console_log(std::clog);
+  consoleSink->set_formatter(logFmt);
+
+  /* fs sink */
+  auto fsSink = boost::log::add_file_log(
+    boost::log::keywords::file_name = "test_%Y-%m-%d_%H-%M-%S.%N.log",
+    boost::log::keywords::rotation_size = 10 * 1024 * 1024,
+    boost::log::keywords::min_free_space = 30 * 1024 * 1024,
+    boost::log::keywords::open_mode = std::ios_base::app);
+  fsSink->set_formatter(logFmt);
+  fsSink->locked_backend()->auto_flush(true);
 }
 
 /**
