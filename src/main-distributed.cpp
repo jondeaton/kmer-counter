a802ad3190d63b1b585daa3ee3adc17ebce142d6
Jon Deaton <jonpauldeaton@gmail.com>
2017-11-06 19:19:31 -0800

    trying to debug

--- src/main-distributed.cpp
+++ src/main-distributed.cpp
@@ -13,6 +13,15 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
+
+  int i = 0;
+  char hostname[256];
+  gethostname(hostname, sizeof(hostname));
+  printf("PID %d on %s ready for attach\n", getpid(), hostname);
+  fflush(stdout);
+  while (0 == i)
+    sleep(5);
+
   DistributedKmerCounter counter(&argc, &argv);
   counter.run();
 }
